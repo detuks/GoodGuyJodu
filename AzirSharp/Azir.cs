339414abb1319aaa44ab854e55083dcb82a3da60
reinis muiz <raimen12@gmail.com>
2015-05-02 13:43:17 +0300

    Azir release

---
+++ AzirSharp/Azir.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+using Collision = LeagueSharp.Common.Collision;
+
+namespace AzirSharp
+{
+    class Azir
+    {
+
+        public static Obj_AI_Hero Player = ObjectManager.Player;
+
+
+        public static SummonerItems sumItems = new SummonerItems(Player);
+
+        public static Spellbook sBook = Player.Spellbook;
+
+        public static Orbwalking.Orbwalker orbwalker;
+
+        public static SpellDataInst Qdata = sBook.GetSpell(SpellSlot.Q);
+        public static SpellDataInst Wdata = sBook.GetSpell(SpellSlot.W);
+        public static SpellDataInst Edata = sBook.GetSpell(SpellSlot.E);
+        public static SpellDataInst Rdata = sBook.GetSpell(SpellSlot.R);
+        public static Spell Q = new Spell(SpellSlot.Q, 750);
+        public static Spell W = new Spell(SpellSlot.W, 450);
+        public static Spell E = new Spell(SpellSlot.E, 650);
+        public static Spell R = new Spell(SpellSlot.R, 250);
+
+        public static List<Obj_AI_Minion> MySoldiers = new List<Obj_AI_Minion>(); 
+
+        public static void setSkillShots()
+        {
+            Q.SetSkillshot(0.0f, 65f, 1500f, false, SkillshotType.SkillshotLine);
+            E.SetSkillshot(0.0f, 65f, 1500f, false, SkillshotType.SkillshotLine);
+        }
+
+        public static List<Obj_AI_Minion> getUsableSoliders()
+        {
+            return MySoldiers.Where(sol => !sol.IsDead).ToList();
+        }
+
+        public static void doCombo(Obj_AI_Hero targ)
+        {
+            if (Player.IsDead)
+                return;
+            if (AzirSharp.Config.Item("useW").GetValue<bool>())
+                castWTarget(targ);
+            // if (getEnemiesInSolRange().Count == 0)
+            if (AzirSharp.Config.Item("useQ").GetValue<bool>())
+                castQTarget(targ);
+
+            if (AzirSharp.Config.Item("useE").GetValue<bool>())
+                castETarget(targ);
+        }
+
+        public static void doAttack()
+        {
+            List<Obj_AI_Hero> enes = getEnemiesInSolRange();
+            if (enes != null)
+            {
+                 foreach (var ene in enes)
+                 {
+
+                     if (Orbwalking.CanMove(0) && Orbwalking.CanAttack() && solisAreStill())
+                     {
+                         Orbwalking.LastAATick = Environment.TickCount;
+                         Player.IssueOrder(GameObjectOrder.AttackUnit, ene);
+                     }
+                 }
+            }
+        }
+
+        public static void castQTarget(Obj_AI_Hero target)
+        {
+            if (!Q.IsReady())
+                return;
+
+            try
+            {
+
+                if (getMiddleDelay(target) != -1)
+                {
+                    PredictionOutput po2 = Prediction.GetPrediction(target, getMiddleDelay(target)*1.1f);
+                    PredictionOutput po = Q.GetPrediction(target);
+                    if (po2.Hitchance > HitChance.Low)
+                    {
+                        Q.Cast(po2.UnitPosition);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        public static void castWTarget(Obj_AI_Hero target)
+        {
+            if (!W.IsReady() && Wdata.Ammo == 0)
+                return;
+            PredictionOutput po = Prediction.GetPrediction(target, 0.2f);
+            W.Cast(po.UnitPosition);
+
+        }
+
+        public static void castETarget(Obj_AI_Hero target)
+        {
+            if (!E.IsReady())
+                return;
+
+            List<Obj_AI_Minion> solis = getUsableSoliders().Where(sol => !sol.IsMoving).ToList();
+            if (solis.Count == 0)
+                return;
+            foreach (var sol in solis)
+            {
+                float toSol = Player.Distance(sol.Position);
+
+                //Collision.GetCollision(new List<Vector3>{sol.Position},getMyEPred(sol));
+                PredictionOutput po = Prediction.GetPrediction(target,toSol/1500f);
+
+
+                if (sol.Distance(po.UnitPosition)<325 && interact(Player.Position.To2D(), sol.Position.To2D(), po.UnitPosition.To2D(), 65) 
+                    && interactsOnlyWithTarg(target,sol,Player.Distance(po.UnitPosition)))
+                {
+                    E.Cast(sol.Position);
+                    return;
+                }
+
+
+                /*if (po.CollisionObjects.Count == 0)
+                    continue;
+                Console.WriteLine(po.CollisionObjects.Count);
+                Obj_AI_Base col = po.CollisionObjects.OrderBy(obj => obj.Distance(Player.Position)).First();
+                if (col.NetworkId == target.NetworkId)
+                {
+                    E.Cast(sol);
+                    return;
+                }*/
+
+            }
+        }
+
+        public static bool interactsOnlyWithTarg(Obj_AI_Hero target,Obj_AI_Base sol, float distColser)
+        {
+            foreach (var hero in ObjectManager.Get<Obj_AI_Hero>().Where(obj => obj.IsValid && obj.IsEnemy && obj.NetworkId != target.NetworkId))
+            {
+                float myDistToIt = Player.Distance(hero);
+                PredictionOutput po = Prediction.GetPrediction(hero, myDistToIt/1500f);
+                if (myDistToIt < distColser &&
+                    interact(sol.Position.To2D(), Player.Position.To2D(), po.UnitPosition.To2D(), 65))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool interact(Vector2 p1, Vector2 p2, Vector2 pC, float radius)
+        {
+
+            Vector2 p3 = new Vector2();
+            p3.X = pC.X + radius;
+            p3.Y = pC.Y + radius;
+            float m = ((p2.Y - p1.Y) / (p2.X - p1.X));
+            float Constant = (m * p1.X) - p1.Y;
+
+            float b = -(2f * ((m * Constant) + p3.X + (m * p3.Y)));
+            float a = (1 + (m * m));
+            float c = ((p3.X * p3.X) + (p3.Y * p3.Y) - (radius * radius) + (2f * Constant * p3.Y) + (Constant * Constant));
+            float D = ((b * b) - (4f * a * c));
+            if (D > 0)
+            {
+                return true;
+            }
+            else
+                return false;
+
+        }
+
+        public static float getMiddleDelay(Obj_AI_Hero target)
+        {
+            float allRange = 0;
+            List<Obj_AI_Minion> solis = getUsableSoliders().Where(sol => (sol.Distance(target.ServerPosition)>325 
+                || sol.Distance(Prediction.GetPrediction(target,0.7f).UnitPosition)>325)).ToList();
+            if (solis.Count == 0)
+                return -1;
+            foreach (var sol in solis)
+            {
+                float dist = sol.Distance(target.ServerPosition);
+                allRange += dist;
+            }
+            return (allRange/(1500f*solis.Count));
+        }
+
+        public static PredictionInput getMyEPred(Obj_AI_Base sol)
+        {
+            PredictionInput pi = new PredictionInput();
+            pi.Aoe = false;
+            pi.Collision = true;
+            pi.Delay = 0.0f;
+            pi.From = Player.ServerPosition;
+            pi.Radius = 65f;
+            pi.Range = 1030f;
+            pi.RangeCheckFrom = Player.ServerPosition;
+            pi.Speed = 1500f;
+            pi.Unit = sol;
+            pi.Type = SkillshotType.SkillshotLine;
+            pi.UseBoundingRadius = false;
+            pi.CollisionObjects = new[]{ CollisionableObjects.Heroes};
+            return pi;
+        }
+
+        public static PredictionInput getSoliderPred(Obj_AI_Base sol, Obj_AI_Hero target)
+        {
+            PredictionInput pi = new PredictionInput();
+            pi.Aoe = true;
+            pi.Collision = false;
+            pi.Delay = 0.0f;
+            pi.From = sol.ServerPosition;
+            pi.Radius = 65f;
+            pi.Range = 830f;
+            pi.RangeCheckFrom = Player.ServerPosition;
+            pi.Speed = 1500f;
+            pi.Unit = target;
+            pi.Type = SkillshotType.SkillshotLine;
+            pi.UseBoundingRadius = true;
+            return pi;
+        }
+
+        public static bool solisAreStill()
+        {
+            List<Obj_AI_Minion> solis = getUsableSoliders();
+            foreach (var sol in solis)
+            {
+                if (sol.IsWindingUp)
+                {
+                   // Console.WriteLine("isAuta awdawdAWD");
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+        public static List<Obj_AI_Hero> getEnemiesInSolRange()
+        {
+            List<Obj_AI_Minion> solis = getUsableSoliders();
+            List<Obj_AI_Hero> enemies = ObjectManager.Get<Obj_AI_Hero>().Where(ene => ene.IsEnemy && ene.IsVisible && !ene.IsDead).ToList();
+            List<Obj_AI_Hero> inRange = new List<Obj_AI_Hero>();
+
+            if (solis.Count == 0)
+                return null;
+            foreach (var ene in enemies)
+             {
+                foreach (var sol in solis)
+                {
+                    if (ene.Distance(sol) < 350)
+                    {
+                        inRange.Add(ene);
+                        break;
+                    }
+                }
+            }
+            return inRange;
+        }
+
+    }
+}
---
+++ AzirSharp/AzirSharp.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeagueSharp;
+using LeagueSharp.Common;
+using System.Drawing;
+/*
+ * ToDo:
+ * 
+ * 
+ * check if in any my minions range <done
+ * 
+ * 
+ * 
+ * dont W far if Q not rdy
+ * 
+ * smart laneClear + good calc
+ * 
+ * smarter E
+ * 
+ * calc extended w aa range on other    
+ * 
+ * W+Q ks
+ * 
+ * 
+ * */
+using SharpDX;
+
+
+namespace AzirSharp
+{
+    internal class AzirSharp
+    {
+
+        public const string CharName = "Azir";
+
+        public static Menu Config;
+
+        public static HpBarIndicator hpi = new HpBarIndicator();
+
+        public AzirSharp()
+        {
+            Console.WriteLine("Azir started");
+            /* CallBAcks */
+            CustomEvents.Game.OnGameLoad += onLoad;
+
+        }
+
+        private static void onLoad(EventArgs args)
+        {
+
+            Game.PrintChat("Azir - Sharp by DeTuKs");
+
+            try
+            {
+
+                Config = new Menu("Azir - Sharp", "Azir", true);
+                //Orbwalker
+                Config.AddSubMenu(new Menu("Orbwalker", "Orbwalker"));
+                Azir.orbwalker = new Orbwalking.Orbwalker(Config.SubMenu("Orbwalker"));
+                //TS
+                var TargetSelectorMenu = new Menu("Target Selector", "Target Selector");
+                TargetSelector.AddToMenu(TargetSelectorMenu);
+                Config.AddSubMenu(TargetSelectorMenu);
+                //Combo
+                Config.AddSubMenu(new Menu("Combo Sharp", "combo"));
+                Config.SubMenu("combo").AddItem(new MenuItem("useQ", "use Q")).SetValue(true);
+                Config.SubMenu("combo").AddItem(new MenuItem("useW", "use W")).SetValue(true);
+                Config.SubMenu("combo").AddItem(new MenuItem("useE", "use E")).SetValue(true);
+
+                //LastHit
+                Config.AddSubMenu(new Menu("LastHit Sharp", "lHit"));
+               
+                //LaneClear
+                Config.AddSubMenu(new Menu("LaneClear Sharp", "lClear"));
+               
+                //Harass
+                Config.AddSubMenu(new Menu("Harass Sharp", "harass"));
+               
+                //Extra
+                Config.AddSubMenu(new Menu("Extra Sharp", "extra"));
+                
+
+                //Debug
+                Config.AddSubMenu(new Menu("Debug", "debug"));
+                Config.SubMenu("debug").AddItem(new MenuItem("db_targ", "Debug Target")).SetValue(new KeyBind('T', KeyBindType.Press, false));
+
+
+                Config.AddToMainMenu();
+                Drawing.OnDraw += onDraw;
+                Game.OnUpdate += OnGameUpdate;
+
+                GameObject.OnCreate += OnCreateObject;
+                GameObject.OnDelete += OnDeleteObject;
+                Obj_AI_Base.OnProcessSpellCast += OnProcessSpell;
+
+              //  Game.OnGameSendPacket += OnGameSendPacket;
+               // Game.OnGameProcessPacket += OnGameProcessPacket;
+
+                Azir.setSkillShots();
+            }
+            catch
+            {
+                Game.PrintChat("Oops. Something went wrong with Yasuo- Sharpino");
+            }
+
+        }
+
+        private static void OnGameProcessPacket(GamePacketEventArgs args)
+        {
+           
+        }
+
+        private static void OnGameSendPacket(GamePacketEventArgs args)
+        {
+            if (args.PacketData[0] == 119)
+                args.Process = false;
+        }
+
+        public static float startTime = 0;
+        public static Vector3 startPos = new Vector3();
+        public static float endTime = 0;
+        public static Vector3 endPos = new Vector3();
+        public static bool first = true;
+
+
+        private static void OnGameUpdate(EventArgs args)
+        {
+            try
+            {
+
+               /* if (Azir.getUsableSoliders().Count != 0)
+                {
+
+                    Obj_AI_Minion fir = Azir.getUsableSoliders().First();
+                    if (fir.IsMoving)
+                    {
+                        if (first)
+                        {
+                            startTime = Game.Time;
+                            startPos = fir.ServerPosition;
+                            first = false;
+                        }
+
+                    }
+                    else
+                    {
+                        if (!first)
+                        {
+                            endTime = Game.Time;
+                            endPos = fir.ServerPosition;
+                            float dist = endPos.Distance(startPos);
+                            Console.WriteLine(dist/(endTime-startTime));
+                            Console.WriteLine(Azir.Player.BoundingRadius);
+                            first = true;
+                        }
+                    }
+                }*/
+
+                if (Azir.orbwalker.ActiveMode.ToString() == "Combo")
+                {
+                    Obj_AI_Hero target = TargetSelector.GetTarget(1000, TargetSelector.DamageType.Magical);
+                    Azir.doAttack();
+                    if(target != null)
+                        Azir.doCombo(target);
+                }
+
+                if (Azir.orbwalker.ActiveMode.ToString() == "Mixed")
+                {
+                    //Azir.doAttack();
+                }
+
+                if (Azir.orbwalker.ActiveMode.ToString() == "LaneClear")
+                {
+                    //Azir.doAttack();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
+        }
+
+        private static void onDraw(EventArgs args)
+        {
+
+        }
+
+        private static void OnCreateObject(GameObject sender, EventArgs args)
+        {
+            if (sender.Name == "AzirSoldier" && sender.IsAlly)
+            {
+                Obj_AI_Minion myMin = sender as Obj_AI_Minion;
+                if (myMin.SkinName == "AzirSoldier")
+                    Azir.MySoldiers.Add(myMin);
+            }
+
+        }
+
+        private static void OnDeleteObject(GameObject sender, EventArgs args)
+        {
+            int i = 0;
+            foreach (var sol in Azir.MySoldiers)
+            {
+                if (sol.NetworkId == sender.NetworkId)
+                {
+                    Azir.MySoldiers.RemoveAt(i);
+                    return;
+                }
+                i++;
+            }
+        }
+
+        public static void OnProcessSpell(Obj_AI_Base obj, GameObjectProcessSpellCastEventArgs arg)
+        {
+
+           
+        }
+
+
+
+
+    }
+}
