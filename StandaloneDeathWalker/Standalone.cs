83fe925a9d562a1d193977c26e2a47b1ed465b1f
reinis muiz <raimen12@gmail.com>
2015-05-09 21:49:18 +0300

    StandAlone DW

---
+++ StandaloneDeathWalker/Standalone.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DetuksSharp;
+using LeagueSharp.Common;
+
+namespace StandaloneDeathWalker
+{
+    class Standalone
+    {
+        public static Menu Config;
+
+        public Standalone()
+        {
+            Config = new Menu("Standalone DW", "standDW", true);
+            Config.AddSubMenu(new Menu("Orbwalker", "Orbwalker"));
+            DeathWalker.AddToMenu(Config.SubMenu("Orbwalker"));
+        }
+    }
+}
